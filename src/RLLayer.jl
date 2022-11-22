f9f106c59f2a88a62485ddae069eca422c795a28
LukasHasf <lukas.hassfurth@uni-jena.de>
2022-11-22 12:15:27 +0100

    Change lucystep

--- src/RLLayer.jl
+++ src/RLLayer.jl
@@ -18,16 +18,16 @@ end
 myconv(a, b, dims) = irfft(rfft(a, dims) .* b, size(a, 1), dims)
 
 function lucystep(e, psf_ft, psf_ft_conj, dims, x)
-    denom = myconv(e, psf_ft_conj, dims)
+    denom = myconv(e, psf_ft, dims)
     fraction = x ./ denom
-    return e .* myconv(fraction, psf_ft, dims)
+    return e .* myconv(fraction, psf_ft_conj, dims)
 end
 
 function (rl::RL)(x; n=30)
     x = anscombe_transform(x)
     dims = 1:(ndims(rl.PSF)-1)
     otf = rfft(rl.PSF, dims)
-    otf_conj = conj.(otf)
+    otf_conj = rfft(reverse(rl.PSF; dims=tuple(collect(dims)...)), dims)
     rec = one.(x)
     for i in 1:n
         rec = lucystep(rec, otf, otf_conj, dims, x)
