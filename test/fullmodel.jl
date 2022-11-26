e6aa4f4e4c6c240c55fd4b58c8ee1ca07fad5cc8
LukasHasf <lukas.hassfurth@uni-jena.de>
2022-11-26 17:20:20 +0100

    Add test for saving / loading `RL_FLFM` networks

--- test/fullmodel.jl
+++ test/fullmodel.jl
@@ -192,3 +192,29 @@ end
     img = rand(Float32, Ny, Nx, nrch, batchsize)
     @test model(img) == loaded_model(img)
 end
+
+@testset "Saving / loading RLLayer_FLFM" begin
+    model_settings = Dict{Symbol, Any}(:attention=>false, :dropout=>false, :depth=>3, :separable=>false, :final_attention=>false, :multiscale=>false, :deconv=>"rl_flfm")
+    Ny = 16
+    Nx = 16
+    Nz = 16
+    nrch = 1
+    nrPSFs = 3
+    batchsize = 1
+    psfs = my_gpu(rand(Float32, Ny, Nx, Nz, nrPSFs))
+    model = my_gpu(SpatiallyVaryingDeconvolution.make_model(psfs, model_settings))
+    @test model[1] isa SpatiallyVaryingDeconvolution.RLLayer_FLFM.RL_FLFM
+    testsave_path = SpatiallyVaryingDeconvolution.save_model(
+        model, mktempdir(), [0.0], 1, 0
+    )
+    loaded_model = SpatiallyVaryingDeconvolution.load_model(
+        testsave_path; load_optimizer=false
+    )
+    @test loaded_model[1] isa SpatiallyVaryingDeconvolution.RLLayer_FLFM.RL_FLFM
+    loaded_model = cpu(loaded_model)
+    model = cpu(model)
+    @test loaded_model[1].PSF == model[1].PSF
+    @test loaded_model[1].n_iter == model[1].n_iter
+    img = rand(Float32, Ny, Nx, nrch, batchsize)
+    @test model(img) == loaded_model(img)
+end
